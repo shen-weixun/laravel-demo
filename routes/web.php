932d41f48bfe1608e95bda3f0d9a18c1a4050215
shen-weixun <shenweixun11@gmail.com>
2026-03-19 05:46:49 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $posts=[];
+    if(auth()->check()){
+     $posts=auth()->user()->useCoolPosts()->latest()->get();
+    }
+    //$posts=Post::where('user_id',auth()->id())->get();
+    return view('home',['posts'=>$posts]);
+});
+
+Route::post('/register', [UserController::class,'register']);
+Route::post('/logout',[UserController::class,'logout']);
+Route::post('/login',[UserController::class,'login']);
+
+
+//Blog post related routes
+Route::post('/create-post',[PostController::class,'createPost']);
+Route::get('/edit-post/{post}',[PostController::class,'showEditScreen']);
+Route::put('/edit-post/{post}',[PostController::class,'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}',[PostController::class,'deletePost']);
